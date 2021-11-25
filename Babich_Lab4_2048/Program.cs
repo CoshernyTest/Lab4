d580378c1c8a80af51af783fe81b1313e497e014
IvessJohn <94851734+CoshernyTest@users.noreply.github.com>
2021-11-25 15:26:28 +0200

    Wrote the ShiftableField class

--- Babich_Lab4_2048/Program.cs
+++ Babich_Lab4_2048/Program.cs
@@ -28,12 +28,12 @@ using (StreamReader sr = new StreamReader(path_in, System.Text.Encoding.Default)
 
 #region ОБРАБОТКА ИНФОРМАЦИИ
 
-int[,] field = initial_field;
-//Console.WriteLine("START FIELD:\n" + GetMatrixInText(field));
+ShiftableField field = new ShiftableField(initial_field);
+//Console.WriteLine("START FIELD:\n" + field.GetMatrixInText(field.Field));
 foreach (string dir in move_dirs)
 {
-    field = MoveFieldInDir(field, dir);
-    //Console.WriteLine(dir + "\n" + GetMatrixInText(field));
+    field.Field = field.MoveFieldInDir(field.Field, dir);
+    //Console.WriteLine(dir + "\n" + GetMatrixInText(field.Field));
 }
 //Console.WriteLine('\n');
 
@@ -42,7 +42,8 @@ foreach (string dir in move_dirs)
 #region ЗАПИСЬ/ВЫВОД
 using (StreamWriter sw = new StreamWriter(path_out, false, System.Text.Encoding.Default))
 {
-    string matrix_in_text = GetMatrixInText(field);
+    string matrix_in_text = Converter.GetMatrixInText(field.Field);
+    
     //Console.WriteLine("FINAL:\n" + matrix_in_text);
     sw.Write(matrix_in_text);
 }
@@ -50,102 +51,6 @@ using (StreamWriter sw = new StreamWriter(path_out, false, System.Text.Encoding.
 
 
 #region МЕТОДЫ
-
-//
-// Сдвигает всю матрицу
-//
-int[,] MoveFieldInDir(int[,] field, string dir)
-{
-    int[,] new_field = field;
-
-    switch (dir)
-    {
-        case "L":
-        case "R":
-            for (int y = 0; y < 4; y++)
-            {
-                int[] line = { field[0, y], field[1, y], field[2, y], field[3, y] };  // Собираем ряд чисел
-                int[] new_line = ShiftLineInDirection(line, dir);     // Сдвигаем
-
-                for (int x = 0; x < 4; x++) { new_field[x, y] = new_line[x]; } // Присваем обновляем новое поле
-            }
-            break;
-        case "U":
-        case "D":
-            for (int x = 0; x < 4; x++)
-            {
-                int[] line = { field[x, 0], field[x, 1], field[x, 2], field[x, 3] };  // Собираем ряд чисел
-                int[] new_line = ShiftLineInDirection(line, dir);     // Сдвигаем
-
-                for (int y = 0; y < 4; y++) { new_field[x, y] = new_line[y]; } // Присваем обновляем новое поле
-            }
-            break;
-    }
-
-    return new_field;
-}
-
-//
-// Сдвигает линию и добавляет 0 с нужной стороны
-//
-int[] ShiftLineInDirection(int[] line, string dir)
-{
-    List<int> result = ShiftLine(line);
-
-    if (result.Count < 4)
-    {
-        int zeros = 4 - result.Count;
-        switch (dir)
-        {
-            case "L":   // Left
-            case "U":   // Up
-                for (int i = 0; i < zeros; i++) { result.Add(0); }
-                break;
-
-            case "R":   // Right
-            case "D":   // Down
-                for (int i = 0; i < zeros; i++) { result.Insert(0, 0); }
-                break;
-        }
-    }
-
-    return result.ToArray();
-}
-
-//
-// Непосредственно сам сдвиг линии
-//
-List<int> ShiftLine(int[] line)
-{
-    List<int> line_list = new List<int>(line);
-    List<int> result = new List<int>();
-
-    line_list.RemoveAll(x => x == 0);
-
-    if (line_list.Count > 0)
-    {
-        if (line_list.Count == 1) { return line_list; }
-        else
-        {
-            for (int i = 0; i < line_list.Count - 1; i++)
-            {
-                if (line_list[i] == line_list[i + 1])
-                {
-                    result.Add(line_list[i] * 2);
-                    i += 1;
-                }
-                else
-                {
-                    result.Add(line_list[i]);
-                    if (i == line_list.Count - 2) { result.Add(line_list[i + 1]); }
-                }
-            }
-        }
-    }
-
-
-    return result;
-}
 
 // Неиспользуемый метод. Первый прототип ShiftLine()
 // Работает только для сдвига строки влево.
@@ -204,21 +109,129 @@ int[] Obsolete_ShiftLineToLeft(int[] nums)
     return nums;
 }
 
-string GetMatrixInText(int[,] field)
-// Превращает матрицу int в одну понятную string
+public class ShiftableField
 {
-    string text = "";
-    
-    for (int y = 0; y < 4; y++)
+    private int[,] field;
+    public int[,] Field { get => field; set => field = value; }
+
+    public ShiftableField() { field = new int[4, 4]; }
+    public ShiftableField(int[,] _field)
+    { field = _field; }
+    //
+    // Сдвигает всю матрицу
+    //
+    public int[,] MoveFieldInDir(int[,] field, string dir)
     {
-        for (int x = 0; x < 4; x++)
+        int[,] new_field = field;
+
+        switch (dir)
         {
-            text += field[x, y].ToString() + " ";
+            case "L":
+            case "R":
+                for (int y = 0; y < 4; y++)
+                {
+                    int[] line = { field[0, y], field[1, y], field[2, y], field[3, y] };  // Собираем ряд чисел
+                    int[] new_line = ShiftLineInDirection(line, dir);     // Сдвигаем
+
+                    for (int x = 0; x < 4; x++) { new_field[x, y] = new_line[x]; } // Присваем обновляем новое поле
+                }
+                break;
+            case "U":
+            case "D":
+                for (int x = 0; x < 4; x++)
+                {
+                    int[] line = { field[x, 0], field[x, 1], field[x, 2], field[x, 3] };  // Собираем ряд чисел
+                    int[] new_line = ShiftLineInDirection(line, dir);     // Сдвигаем
+
+                    for (int y = 0; y < 4; y++) { new_field[x, y] = new_line[y]; } // Присваем обновляем новое поле
+                }
+                break;
         }
 
-        text += "\r\n";
+        return new_field;
     }
 
-    return text;
+    //
+    // Сдвигает линию и добавляет 0 с нужной стороны
+    //
+    public int[] ShiftLineInDirection(int[] line, string dir)
+    {
+        List<int> result = ShiftLine(line);
+
+        if (result.Count < 4)
+        {
+            int zeros = 4 - result.Count;
+            switch (dir)
+            {
+                case "L":   // Left
+                case "U":   // Up
+                    for (int i = 0; i < zeros; i++) { result.Add(0); }
+                    break;
+
+                case "R":   // Right
+                case "D":   // Down
+                    for (int i = 0; i < zeros; i++) { result.Insert(0, 0); }
+                    break;
+            }
+        }
+
+        return result.ToArray();
+    }
+
+    //
+    // Непосредственно сам сдвиг линии
+    //
+    public List<int> ShiftLine(int[] line)
+    {
+        List<int> line_list = new List<int>(line);
+        List<int> result = new List<int>();
+
+        line_list.RemoveAll(x => x == 0);
+
+        if (line_list.Count > 0)
+        {
+            if (line_list.Count == 1) { return line_list; }
+            else
+            {
+                for (int i = 0; i < line_list.Count - 1; i++)
+                {
+                    if (line_list[i] == line_list[i + 1])
+                    {
+                        result.Add(line_list[i] * 2);
+                        i += 1;
+                    }
+                    else
+                    {
+                        result.Add(line_list[i]);
+                        if (i == line_list.Count - 2) { result.Add(line_list[i + 1]); }
+                    }
+                }
+            }
+        }
+
+
+        return result;
+    }
+}
+
+public class Converter
+{
+    public static string GetMatrixInText(int[,] field)
+    // Превращает матрицу int в одну понятную string
+    {
+        string text = "";
+
+        for (int y = 0; y < 4; y++)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                text += field[x, y].ToString() + " ";
+            }
+
+            text += "\r\n";
+        }
+
+        return text;
+    }
 }
 #endregion
