0178ff3c7fb95add73ce9b20bf6dfc7e25f48ba8
IvessJohn <94851734+CoshernyTest@users.noreply.github.com>
2021-11-26 06:30:18 +0200

    Update UnitTest.cs

--- TestProject/UnitTest.cs
+++ TestProject/UnitTest.cs
@@ -21,13 +21,85 @@ namespace TestProject
                 "0 0 0 0\r\n" +
                 "0 0 0 0\r\n" +
                 "0 0 0 0";
-            
+
             ShiftableField field = new ShiftableField(Converter.GetTextToShiftableFieldField(input.Split("\r\n")));
             field.Field = field.ShiftFieldInDirections(field.Field, dirs_list);
 
             string actual = Converter.GetMatrixInText(field.Field);
 
             Assert.AreEqual(expected, actual, "TEST 1 FAILED");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            string input = "" +
+                "1 1 1 1\r\n" +
+                "0 0 0 0\r\n" +
+                "0 0 0 0\r\n" +
+                "0 0 0 0";
+            List<string> dirs_list = new List<string> { "R", "D", "L", "U" };
+
+            string expected = "" +
+                "4 0 0 0\r\n" +
+                "0 0 0 0\r\n" +
+                "0 0 0 0\r\n" +
+                "0 0 0 0";
+
+            ShiftableField field = new ShiftableField(Converter.GetTextToShiftableFieldField(input.Split("\r\n")));
+            field.Field = field.ShiftFieldInDirections(field.Field, dirs_list);
+
+            string actual = Converter.GetMatrixInText(field.Field);
+
+            Assert.AreEqual(expected, actual, "TEST 2 FAILED");
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            string input = "" +
+                "0 0 0 0\r\n" +
+                "2 0 2 0\r\n" +
+                "0 0 0 0\r\n" +
+                "0 4 0 8";
+            List<string> dirs_list = new List<string> { "R", "U", "R"};
+
+            string expected = "" +
+                "0 0 0 8\r\n" +
+                "0 0 0 8\r\n" +
+                "0 0 0 0\r\n" +
+                "0 0 0 0";
+
+            ShiftableField field = new ShiftableField(Converter.GetTextToShiftableFieldField(input.Split("\r\n")));
+            field.Field = field.ShiftFieldInDirections(field.Field, dirs_list);
+
+            string actual = Converter.GetMatrixInText(field.Field);
+
+            Assert.AreEqual(expected, actual, "TEST 3 FAILED");
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            string input = "" +
+                "0 0 0 0\r\n" +
+                "2 0 2 0\r\n" +
+                "1 1 1 1\r\n" +
+                "0 4 0 8";
+            List<string> dirs_list = new List<string> { "R"};
+
+            string expected = "" +
+                "0 0 0 0\r\n" +
+                "0 0 0 4\r\n" +
+                "0 0 2 2\r\n" +
+                "0 0 4 8";
+
+            ShiftableField field = new ShiftableField(Converter.GetTextToShiftableFieldField(input.Split("\r\n")));
+            field.Field = field.ShiftFieldInDirections(field.Field, dirs_list);
+
+            string actual = Converter.GetMatrixInText(field.Field);
+
+            Assert.AreEqual(expected, actual, "TEST 4 FAILED");
         }
     }
 }
